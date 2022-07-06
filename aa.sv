f31c7f4979583964b1ffe1f34ed4319a76ba2c06
Ben Guan <42010432+Jguan2014@users.noreply.github.com>
2022-07-06 11:45:03 -0700

    Update aa.sv

--- aa.sv
+++ aa.sv
@@ -10,27 +10,37 @@ module aa(
   
   
 
-  logic [9:0] row, col;
+
   logic [size-1: 0] avg_frame [size-1:0],
+  genvar row, col;
+generate
+for (i = 0; i < 3 ; i = i + 1) begin: 
+    always @(posedge sysclk) begin
+        temp[i] <= 1'b0;
+    end
+end
+endgenerate
+
   
-  always @(posedge clk) begin 
+  always @(posedge clk or negedge reset_n) begin 
     if (!reset_n) begin
         row = 1;
         col = 1;
     end else begin 
-      if row <size-1 begin 
-        if col < size - 1 begin 
-          if (frame[row][col] > th && (frame[row+1][col] < th || frame[row-1][col] < th || frame[row][col+1] < th || frame[row][col-1] < th)) begin 
-            avg_frame[row][col] <= (frame[row][col]+frame[row+1][col]+frane[row-1][col]+frame[row][col+1]+frame[row][col-1])>>4 //if it is a black pixel neighbours a white pixel
-            //calculate the avg of the neighbouring pixels and divide by 4, shift right twice is easier/efficient that divide by 5
-          end 
+      generate 
+        for (row = 0; row <size-1; row= row+1) begin 
+          for (col = 0; col <size-1; col= col+1) begin 
 
-          col <= col +1;
+              if (frame[row][col] > th && (frame[row+1][col] < th || frame[row-1][col] < th || frame[row][col+1] < th || frame[row][col-1] < th)) begin 
+                avg_frame[row][col] <= (frame[row][col]+frame[row+1][col]+frane[row-1][col]+frame[row][col+1]+frame[row][col-1])>>4 //if it is a black pixel neighbours a white pixel
+                //calculate the avg of the neighbouring pixels and divide by 4, shift right twice is easier/efficient that divide by 5
+              end 
+            end 
           end 
-        row <= row +1; 
-        end 
+      endgenerate 
+    end 
   end 
     
-    assign done = (row == size-1 && col ==1)? 1 : 0; //assert done when the whole frame has been processed 
+      assign done = (row == size-1 && col == size-1)? 1 : 0; //assert done when the whole frame has been processed 
    
     endmodule 
