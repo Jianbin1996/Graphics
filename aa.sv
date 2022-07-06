5dd18b463aaa89ace898469e99f73affcfe2d2bd
Ben Guan <42010432+Jguan2014@users.noreply.github.com>
2022-07-06 11:20:45 -0700

    Update aa.sv

--- aa.sv
+++ aa.sv
@@ -18,14 +18,17 @@ module aa(
         row = 1;
         col = 1;
     end else begin 
-      if row <size-1 
-        if col < size - 1 
-          if (frame[row][col] > th && (frame[row][col] < th || frame[row][col] < th )  )
+      if row <size-1 begin 
+        if col < size - 1 begin 
+          if (frame[row][col] > th && (frame[row][col] < th || frame[row][col] < th )) begin 
             avg_frame[row][col] <= (frame[row][col]+frame[row+1][col]+frane[row-1][col]+frame[row][col+1]+frame[row][col-1])>>4 //if it is a black pixel neighbours a white pixel
             //calculate the avg of the neighbouring pixels and divide by 4 
-            row <= row +1;
-            col <= col +1;
-    
+          end 
+
+          col <= col +1;
+          end 
+        row <= row +1; 
+        end 
   end 
     
     assign done = (row == size-1 && col ==1)? 1 : 0; //assert done when the whole frame has been processed 
