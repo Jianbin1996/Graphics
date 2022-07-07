56be7ac0437bf6f9defaa5859ee5b738a1c9b13e
Ben Guan <42010432+Jguan2014@users.noreply.github.com>
2022-07-07 15:27:19 -0700

    Update aa.sv

--- aa.sv
+++ aa.sv
@@ -1,20 +1,15 @@
 //Pseudocode for anti-alising FPGA implementation 
 
-module aa(
-  logic clk,
-  logic reset_n,
-  logic [size-1: 0] frame [size-1:0],
-  logic th,
-  logic done
+module aa(parameter bitwidth = 10)(
+  logic                 clk,
+  logic                 reset_n,
+  logic [size-1: 0]     frame [size-1:0],
+  logic [bitwidth-1:0]  th,
+  logic                 done
 );
-  
-  
-
-
+ 
   logic [size-1: 0] avg_frame [size-1:0],
   genvar row, col;
-
-
   
   always @(posedge clk or negedge reset_n) begin 
     if (!reset_n) begin
@@ -24,7 +19,6 @@ module aa(
       generate 
         for (row = 0; row < size-1; row = row+1) begin 
           for (col = 0; col < size-1; col = col+1) begin 
-
               if (frame[row][col] > th && (frame[row+1][col] < th || frame[row-1][col] < th || frame[row][col+1] < th || frame[row][col-1] < th)) begin 
                 avg_frame[row][col] <= (frame[row][col] + frame[row+1][col] + frame[row-1][col] + frame[row][col+1] + frame[row][col-1]) >> 4 //if it is a black pixel neighbours a white pixel
                 //calculate the avg of the neighbouring pixels and divide by 4, shift right twice is easier/efficient that divide by 5
